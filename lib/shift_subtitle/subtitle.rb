f0f4cf5ca6509fe2b68df0b2e86f6aeab0ec97c3
Felipe Elias Philipp <felipeelias@gmail.com>
2009-10-01 23:11:04 -0300

    Now class Subtitle uses Option class

--- lib/shift_subtitle/subtitle.rb
+++ lib/shift_subtitle/subtitle.rb
@@ -1,4 +1,4 @@
-require "time"
+require 'time'
 
 class Time
   def print_time
@@ -7,20 +7,23 @@ class Time
 end
 
 module ShiftSubtitle
-  class Subtitle
-    attr_reader :subtitle_path, :output_path, :operation, :shift
-    
-    def initialize(subtitle_path, output_path, shift, operation = :+)
-      @subtitle_path, @output_path, @shift, @operation = subtitle_path, output_path, shift, operation
-    end
-    
-    def convert!
-      input_file  = File.read(subtitle_path)
-      output_file = File.new(output_path, "w")
+  class Subtitle    
+    def self.run!(args)
+      @options = ShiftSubtitle::Options.new(args)
       
-      output_file << input_file.gsub(/^([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> ([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\s$/) do
+      input_file  = File.read(@options[:input_file])
+      output_file = File.new(@options[:output_file], "w")
+      
+      operation = case @options[:operation]
+        when :add then :+
+        else :-
+      end
+      
+      puts @options.inspect
+      
+      output_file << input_file.gsub(/^([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> ([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})/) do
         time_start, time_end = Time.parse($1), Time.parse($2)
-        time_start, time_end = time_start.send(operation, shift), time_end.send(operation, shift)
+        time_start, time_end = time_start.send(operation, @options[:time]), time_end.send(operation, @options[:time])
         "#{time_start.print_time} --> #{time_end.print_time}"
       end
       
@@ -28,10 +31,3 @@ module ShiftSubtitle
     end      
   end  
 end
-
-shift_time = 1.100
-
-input   = File.join(File.dirname(__FILE__), "subtitle.srt")
-output  = File.join(File.dirname(__FILE__), "subtitle_output.srt")
-
-ShiftSubtitle::Subtitle.new(input, output, shift_time, :-).convert!
